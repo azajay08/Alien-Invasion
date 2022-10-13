9a5435e288be178b121a69280d6d29413fed0402
Aaron Jones <aaron.p.j@hotmail.com>
2022-10-14 02:20:19 +0300

    game now speeds up

--- alien_invasion.py
+++ alien_invasion.py
@@ -175,6 +175,7 @@ class AlienInvasion:
 			# Destroy existing belluets and create new fleet
 			self.bullets.empty()
 			self._create_fleet()
+			self.settings.increase_speed()
 
 	def _update_stars(self):
 		"""Update star pos"""
@@ -202,6 +203,8 @@ class AlienInvasion:
 		"""Start a new game when the player clock Play."""
 		button_clicked = self.play_button.rect.collidepoint(mouse_pos)
 		if button_clicked and not self.stats.game_active:
+			# Reset the game settings
+			self.settings.initialize_dynamic_settings()
 			self.stats.reset_stats()
 			self.stats.game_active = True
 
--- settings.py
+++ settings.py
@@ -12,28 +12,25 @@ class Settings:
 	"""A class to store all the settings for Alien Invasion"""
 
 	def __init__(self):
-		"""Initialize the game's settings"""
+		"""Initialize the game's static settings"""
 		# Screen settings
 		self.screen_width = 1100
 		self.screen_height = 700
 		self.bg_colour = black
 
 		# ship settings
-		self.ship_speed = 2
+		
 		self.ship_limit = 3
 
 		# Bullet settings
-		self.bullet_speed = 3.0
 		self.bullet_width = 5
 		self.bullet_height = 15
 		self.bullet_colour = deep_pink
 		self.bullets_allowed = 15
 
 		# Alien settings
-		self.alien_speed = 1.0
 		self.fleet_drop_speed = 10
-		# Fleet direction of 1 represents right: -1 left
-		self.fleet_direction = 1
+		
 
 		# stars
 		self.star_colour = white
@@ -42,3 +39,24 @@ class Settings:
 		self.star_x = 1
 		self.star_y = 1
 		self.star_speed = 0.5
+
+		# How quickly game speeds up
+		self.speedup_scale = 1.1
+
+		self.initialize_dynamic_settings()
+
+
+	def initialize_dynamic_settings(self):
+		"""Init settings that change through the game"""
+		self.ship_speed = 2
+		self.bullet_speed = 3.0
+		self.alien_speed = 1.0
+
+		# Fleet direction of 1 represents right: -1 left
+		self.fleet_direction = 1
+
+	def increase_speed(self):
+		"""Increase speed settings"""
+		self.ship_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.alien_speed *= self.speedup_scale
