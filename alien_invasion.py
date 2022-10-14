2729ce4f87cd565f63c40798f7cdd3a2011c2733
Aaron Jones <aaron.p.j@hotmail.com>
2022-10-14 03:12:28 +0300

    Added high score

--- alien_invasion.py
+++ alien_invasion.py
@@ -178,6 +178,7 @@ class AlienInvasion:
 			for aliens in collisions.values():
 				self.stats.score += self.settings.alien_points * len(aliens)
 			self.sb.prep_score()
+			self.sb.check_high_score()
 		if not self.aliens:
 			# Destroy existing belluets and create new fleet
 			self.bullets.empty()
--- scoreboard.py
+++ scoreboard.py
@@ -21,16 +21,19 @@ class Scoreboard:
 
 		# Font settings for scoring info
 		self.text_colour = yellow
-		self.font = pygame.font.SysFont(None, 48)
+		self.hs_text_colour = deep_pink
+		self.font_score = pygame.font.SysFont(None, 48)
+		self.font_h_score = pygame.font.SysFont(None, 32)
 
 		# Prepare the inital score image
 		self.prep_score()
+		self.prep_high_score()
 
 	def prep_score(self):
 		"""Turn score into rendered image"""
 		rounded_score = round(self.stats.score -1)
 		score_str = "{:,}".format(rounded_score)
-		self.score_image = self.font.render(score_str, True,
+		self.score_image = self.font_score.render(score_str, True,
 						self.text_colour, self.settings.bg_colour)
 		
 		# Display the score at the top right of the screen.
@@ -39,6 +42,25 @@ class Scoreboard:
 		self.score_rect.right = self.screen_rect.right - 20
 		self.score_rect.top = 20
 
+	def prep_high_score(self):
+		"""Turn high score into rendered image"""
+		high_score = round(self.stats.high_score, -1)
+		high_score_str = "High Score:{:,}".format(high_score)
+		self.high_score_image = self.font_h_score.render(high_score_str, True,
+							self.hs_text_colour, self.settings.bg_colour)
+		
+		# Center the high score at the top of the screen
+		self.high_score_rect = self.high_score_image.get_rect()
+		self.high_score_rect.left = self.screen_rect.left + 20
+		self.high_score_rect.top = 20
+
 	def show_score(self):
 		"""Draw score to the screen."""
 		self.screen.blit(self.score_image, self.score_rect)
+		self.screen.blit(self.high_score_image, self.high_score_rect)
+
+	def check_high_score(self):
+		"""Check to see if there is a new high score"""
+		if self.stats.score > self.stats.high_score:
+			self.stats.high_score = self.stats.score
+			self.prep_high_score()
