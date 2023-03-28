a525948de200cf5616597c0910ce5e8f1c593d98
azajay08 <aaron.p.j@hotmail.com>
2023-03-29 01:15:08 +0300

    added a and d for controls

--- alien_invasion.py
+++ alien_invasion.py
@@ -248,6 +248,10 @@ class AlienInvasion:
 			self.ship.moving_right = True
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = True
+		elif event.key == pygame.K_d:
+			self.ship.moving_right = True
+		elif event.key == pygame.K_a:
+			self.ship.moving_left = True
 		# elif event.key == pygame.K_UP:
 		# 	self.ship.moving_up = True
 		# elif event.key == pygame.K_DOWN:
@@ -277,6 +281,10 @@ class AlienInvasion:
 		if event.key == pygame.K_RIGHT:
 			self.ship.moving_right = False
 		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
+		elif event.key == pygame.K_d:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_a:
 			self.ship.moving_left = False
 		# elif event.key == pygame.K_UP:
 		# 	self.ship.moving_up = False
